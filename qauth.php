e75a7afc5258bfa73a3697f72ccd7a991b689989
Jon Wong <JonTheWong@users.noreply.github.com>
2017-06-08 11:51:09 -0400

    updated to reflect difference with eauth
    updated doc.

--- qauth.php
+++ qauth.php
@@ -1,7 +1,7 @@
 <?php
 /*
 
-AutoAuth Invoice Auto Login v1
+AutoAuth Quote Auto Login v1.1
 
 This script is intended to allow your automatic invoice emails from whmcs to include login information.
 This will allow your clients to click on a link in your email and automaticly access the lastest invoice.
@@ -16,12 +16,14 @@ This script was tested on 5.3.6 General Release (5.3.6-release.1)
 To install this script you need to place this file in your root directory of whmcs
 then generate a fresh hash5 and enter it in configuration.php in the root of your whmcs directory.
 $autoauthkey = "yourhash5";
-Then add this value to the top of your email template (i recommend the invoicecreated/invoiceoveride1/2/3 and invoicepaymentreminder themplates)
+Then add this value to the top of your email template
 {assign var='hash' value=$client_email|cat:"same hash 5 as $secret_key in this script"}
 and then add this link anywhere in your template based on your style of choice.
-{$whmcs_url}/eauth.php?email={$client_email}&invoice={$invoice_num}&hash={$hash|md5}
+{$whmcs_url}/qauth.php?email={$client_email}&amp;quote={$quote_number}&amp;hash={$hash|md5}
+
 if you would like to place the link inside of a WORD in your template i'd recommend doing.
-<a href="yourdomain.com/eauth.php?email={$client_email}&invoice={$invoice_num}&hash={$hash|md5}">AUTO INVOICE LOGIN</a>
+<a href="https://www.zenithmedia.ca/qauth.php?email={$client_email}&amp;quote={$quote_number}&amp;hash={$hash|md5}">
+AUTO QUOTE LOGIN</a>
 
 */
 $whmcsurl = "https://www.yoursite.com/whmcs/dologin.php";
