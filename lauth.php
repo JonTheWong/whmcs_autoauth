0364e243d1f8d1c1457d72c8404ea83da2740032
Jon Wong <jon@zenithmedia.ca>
2019-04-26 19:59:41 -0400

    Added Login Auth
    This is to direct the client to the main client area.

---
+++ lauth.php
@@ -0,0 +1,22 @@
+<?php
+/*
+Created by JonTheWong @ Zenith Media Canada - www.zenithmedia.ca
+Visit our public repo - https://github.com/JonTheWong/whmcs-autoauth-invoice
+*/
+
+$whmcsurl = "https://www.zenithmedia.ca/portal/dologin.php"; /* replace with your url */
+$autoauthkey = "REPLACE-WITH-SYSTEM-KEY"; /* same as in configuration.php */
+$secretkey = "REPLACE-THIS-PART-WITH-SECRETE-KEY"; /* generate a new key for this script */
+
+if (md5($_GET['email'].$secretkey) != $_GET['zmkey'])
+die('Visit https://www.zenithmedia.ca/login/ to login manually');
+
+$email = $_GET['email'];
+$timestamp = time();
+$hash = sha1($email.$timestamp.$autoauthkey);
+$goto = "clientarea.php";
+
+$url = $whmcsurl."?email=$email&timestamp=$timestamp&hash=$hash&goto=".urlencode($goto);
+header("Location: $url");
+exit;
+?>
