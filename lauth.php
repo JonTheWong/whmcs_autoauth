25779656a6c8a7f6d5e94591153cde4303d9fff9
Jon Wong <jon@zenithmedia.ca>
2020-03-19 17:11:26 -0400

    Updated Links

--- lauth.php
+++ lauth.php
@@ -4,12 +4,12 @@ Created by JonTheWong @ Zenith Media Canada - www.zenithmedia.ca
 Visit our public repo - https://github.com/JonTheWong/whmcs-autoauth-invoice
 */
 
-$whmcsurl = "https://www.zenithmedia.ca/portal/dologin.php"; /* replace with your url */
+$whmcsurl = "https://zenithmedia.ca/portal/dologin.php"; /* replace with your url */
 $autoauthkey = "REPLACE-WITH-SYSTEM-KEY"; /* same as in configuration.php */
 $secretkey = "REPLACE-THIS-PART-WITH-SECRET-KEY"; /* generate a new key for this script */
 
 if (md5($_GET['email'].$secretkey) != $_GET['zmkey'])
-die('Visit https://www.zenithmedia.ca/login/ to login manually');
+die('Visit https://zenithmedia.ca/login/ to login manually');
 
 $email = $_GET['email'];
 $timestamp = time();
